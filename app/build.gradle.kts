be8a3c75bcc805456c738c3d0077c172c21f8471
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-01-31 22:21:05 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
     namespace = "com.opuside.app"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.opuside.app"
-        minSdk = 36
-        targetSdk = 36
+        minSdk = 35
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
 
