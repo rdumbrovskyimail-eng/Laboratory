d1293cf09f817d1e6327c1599ef87413e3ae571a
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-02-10 18:20:53 +0100

    Delete app/src/main/java/com/example/cache/model/Poem.kt

--- app/src/main/java/com/example/cache/model/Poem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cache.model
-
-data class Poem(
-    val id: String,
-    val title: String,
-    val author: String,
-    val text: String,
-    val cachedAt: Long = System.currentTimeMillis()
-)
