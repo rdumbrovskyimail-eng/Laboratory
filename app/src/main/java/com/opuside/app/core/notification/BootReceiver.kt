3eaccd6e7c8c3a15c4e1463b427c7b5de6cd165e
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-03-10 12:38:12 +0100

    ygh

---
+++ app/src/main/java/com/opuside/app/core/notification/BootReceiver.kt
@@ -0,0 +1,13 @@
+package com.opuside.app.core.notification
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            WorkflowMonitorService.start(context)
+        }
+    }
+}
