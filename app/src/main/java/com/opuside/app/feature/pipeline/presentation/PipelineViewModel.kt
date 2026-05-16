cd339d89217dc2180dec3da50f875bda650bdad6
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-05-16 17:27:02 +0200

    Update file

--- app/src/main/java/com/opuside/app/feature/pipeline/presentation/PipelineViewModel.kt
+++ app/src/main/java/com/opuside/app/feature/pipeline/presentation/PipelineViewModel.kt
@@ -20,6 +20,7 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
